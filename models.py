bada5571e3631849a13bb82f047ded5d4bfc57e4
abdel-csc <abdel.mahmoud@cix.csi.cuny.edu>
2026-03-07 21:12:10 -0500

    Add initial scaffolds for ByteBites classes

--- models.py
+++ models.py
@@ -4,3 +4,32 @@
 # - MenuItem: stores name, price, category, popularity
 # - Menu: holds all MenuItems, supports filtering by category
 # - Order: holds selected MenuItems, computes total cost
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.history = []  # list of Order objects
+
+
+class MenuItem:
+    def __init__(self, name, price, category, popularity):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.popularity = popularity
+
+
+class Menu:
+    def __init__(self):
+        self.items = []  # list of MenuItem objects
+
+    def filter_by_category(self, category):
+        pass  # to be implemented in Part 3
+
+
+class Order:
+    def __init__(self):
+        self.items = []  # list of MenuItem objects
+
+    def compute_total(self):
+        return sum(item.price for item in self.items)
